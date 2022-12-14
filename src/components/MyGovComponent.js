0e8a098e8b1631203930f0341cbaecc81ccafb53
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-14 23:13:53 +0300

    submit project proposal implementation

--- src/components/MyGovComponent.js
+++ src/components/MyGovComponent.js
@@ -1,9 +1,34 @@
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import MyGovAbi from './MyGov.json'; // import the smart contract ABI
+import {
+  Select,
+  Button,
+  TextField,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from '@mui/material';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const MyGovComponent = () => {
-  const [myGovInstance, setMyGovInstance] = useState();
+  const [timeValue, setTimeValue] = useState(dayjs('2014-08-18T21:11:54'));
+  const [timeDiff, setTimeDiff] = useState(0);
+  const [numberOfPayments, setNumberOfPayments] = React.useState(1);
+  const dateNow = dayjs();
+
+  const handlePaymentChange = (event) => {
+    setNumberOfPayments(event.target.value);
+  };
+  const handleTimeChange = (newValue) => {
+    // console.log(newValue, 'newValue');
+    // let timeVal = newValue.split('T')[0];
+    // console.log(timeVal, 'timeVal');
+    // setTimeValue(timeVal);
+    setTimeDiff(newValue.diff(dateNow));
+  };
   window.userAddress = null;
   const contract_address = '0x54c0B35dB02f2E7132c138B0EcC5aFE4b693fEa9';
 
@@ -88,69 +113,112 @@ const MyGovComponent = () => {
     const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
     // console.log(contract);
     try {
-      await contract.submitProjectPropsal(
+      await contract.submitProjectProposal(
         ipfshash,
         votedeadline,
         paymentamounts,
-        payschedule
+        payschedule,
+        { value: ethers.utils.parseEther('0.1') }
       );
     } catch (error) {
-      alert('you have already used the faucet');
+      console.log(error);
+      alert('error in submit project proposal');
     }
   };
   return (
     <div>
-      <button onClick={loginWithEth}>Login</button>
-      <button onClick={faucetHandler}>Use faucet</button>
+      <Button onClick={loginWithEth}>Login</Button>
+      <Button onClick={faucetHandler}>Use faucet</Button>
       <form>
-        <input type='number' id='amount' name='amount' placeholder='amount' />
-        <button
+        <TextField
+          type='number'
+          id='amount'
+          name='amount'
+          placeholder='amount'
+        />
+        <Button
           onClick={(event) =>
             event.preventDefault() ||
             customFaucetHandler(document.getElementById('amount').value)
           }
         >
           Use custom faucet
-        </button>
+        </Button>
       </form>
       <form>
-        <input
+        <TextField
           type='text'
           id='ipfshash'
           name='ipfshash'
           placeholder='ipfshash'
         />
-        <input
-          type='number'
-          id='votedeadline'
-          name='votedeadline'
-          placeholder='votedeadline'
-        />
-        <input
-          type='number'
-          id='paymentamounts'
-          name='paymentamounts'
-          placeholder='paymentamounts'
-        />
-        <input
-          type='number'
-          id='payschedule'
-          name='payschedule'
-          placeholder='payschedule'
-        />
-        <button
-          onClick={(event) =>
-            event.preventDefault() ||
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
+            label='Vote Deadline'
+            inputFormat='DD/MM/YYYY'
+            value={timeValue}
+            id='voteDeadline'
+            onChange={handleTimeChange}
+            renderInput={(params) => <TextField {...params} />}
+            minDate={dateNow}
+          />
+        </LocalizationProvider>
+        <FormControl sx={{ width: 220 }}>
+          <InputLabel id='demo-simple-select-label'>
+            Number Of Payments
+          </InputLabel>
+
+          <Select
+            id='demo-simple-select'
+            value={numberOfPayments}
+            label='Number of payments'
+            onChange={handlePaymentChange}
+          >
+            {Array.from(Array(20).keys()).map((i) => (
+              <MenuItem value={i + 1}>{i + 1}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {Array.from(Array(numberOfPayments).keys()).map((i) => (
+          <div>
+            <TextField
+              type='number'
+              id={'paymentamounts' + (i + 1)}
+              name='paymentamounts'
+              placeholder={'paymentamounts' + (i + 1)}
+            />
+            <TextField
+              type='number'
+              id={'payschedule' + (i + 1)}
+              name='payschedule'
+              placeholder={'payschedule' + (i + 1)}
+            />
+          </div>
+        ))}
+
+        <Button
+          onClick={(event) => {
+            event.preventDefault();
+            let paymentamounts = [];
+            let payschedule = [];
+            for (let i = 0; i < numberOfPayments; i++) {
+              paymentamounts.push(
+                document.getElementById('paymentamounts' + (i + 1)).value
+              );
+              payschedule.push(
+                document.getElementById('payschedule' + (i + 1)).value
+              );
+            }
             submitProjectPropsalHandler(
               document.getElementById('ipfshash').value,
-              document.getElementById('votedeadline').value,
-              document.getElementById('paymentamounts').value,
-              document.getElementById('payschedule').value
-            )
-          }
+              timeDiff,
+              paymentamounts,
+              payschedule
+            );
+          }}
         >
           Submit Project Proposal
-        </button>
+        </Button>
       </form>
     </div>
   );
