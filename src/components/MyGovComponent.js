1296aa7337d35e9f9b784834842e15f26b3c92b9
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-15 00:33:44 +0300

    many functions implemented

--- src/components/MyGovComponent.js
+++ src/components/MyGovComponent.js
@@ -14,8 +14,15 @@ import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const MyGovComponent = () => {
-  const [timeValue, setTimeValue] = useState(dayjs('2014-08-18T21:11:54'));
+  const [timeValue, setTimeValue] = useState(dayjs('2022-12-18T21:11:54'));
+  const [surveyTimeValue, setSurveyTimeValue] = useState(
+    dayjs('2022-12-18T21:11:54')
+  );
   const [timeDiff, setTimeDiff] = useState(0);
+  const [surveyTimeDiff, setSurveyTimeDiff] = useState(0);
+  const [voteChoice, setVoteChoice] = useState();
+  const [votePaymentChoice, setVotePaymentChoice] = useState();
+
   const [numberOfPayments, setNumberOfPayments] = React.useState(1);
   const dateNow = dayjs();
 
@@ -23,11 +30,18 @@ const MyGovComponent = () => {
     setNumberOfPayments(event.target.value);
   };
   const handleTimeChange = (newValue) => {
-    // console.log(newValue, 'newValue');
-    // let timeVal = newValue.split('T')[0];
-    // console.log(timeVal, 'timeVal');
-    // setTimeValue(timeVal);
+    setTimeValue(newValue);
     setTimeDiff(newValue.diff(dateNow));
+  };
+  const handleSurveyTimeChange = (newValue) => {
+    setSurveyTimeValue(newValue);
+    setSurveyTimeDiff(newValue.diff(dateNow));
+  };
+  const handleVoteChange = (event) => {
+    setVoteChoice(event.target.value);
+  };
+  const handleVotePaymentChange = (event) => {
+    setVotePaymentChoice(event.target.value);
   };
   window.userAddress = null;
   const contract_address = '0x54c0B35dB02f2E7132c138B0EcC5aFE4b693fEa9';
@@ -125,6 +139,108 @@ const MyGovComponent = () => {
       alert('error in submit project proposal');
     }
   };
+  const donateEtherHandler = async (value) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.donateEther({ value: ethers.utils.parseEther(value) });
+    } catch (error) {
+      console.log(error);
+      alert('error in donate ether');
+    }
+  };
+  const donateMyGovHandler = async (amount) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.donateMyGovToken(amount);
+    } catch (error) {
+      console.log(error);
+      alert('error in donate mygov token');
+    }
+  };
+  const voteForProjectProposalHandler = async (projectid, vote) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.voteForProjectProposal(projectid, vote);
+    } catch (error) {
+      console.log(error);
+      alert('error in vote for project proposal');
+    }
+  };
+  const voteForProjectPaymentHandler = async (projectid, vote) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.voteForProjectPayment(projectid, vote);
+    } catch (error) {
+      console.log(error);
+      alert('error in vote for project payment');
+    }
+  };
+  const delegateVoteToHandler = async (delegateaddress, projectid) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.delegateVoteTo(delegateaddress, projectid);
+    } catch (error) {
+      console.log(error);
+      alert('error in delegate vote to');
+    }
+  };
+  const submitSurveyHandler = async (
+    ipfshash,
+    surveydeadline,
+    numchoices,
+    atmostchoice
+  ) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //await provider.send("eth_requestAccounts",[]);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contract_address, MyGovAbi, signer);
+    // console.log(contract);
+    try {
+      await contract.submitSurvey(
+        ipfshash,
+        surveydeadline,
+        numchoices,
+        atmostchoice,
+        { value: ethers.utils.parseEther('0.04') }
+      );
+    } catch (error) {
+      console.log(error);
+      alert('error in submit survey');
+    }
+  };
+
   return (
     <div>
       <Button onClick={loginWithEth}>Login</Button>
@@ -218,6 +334,174 @@ const MyGovComponent = () => {
           }}
         >
           Submit Project Proposal
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='number'
+          id='donateEther'
+          name='donateEther'
+          placeholder='donateEther'
+        />
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            donateEtherHandler(document.getElementById('donateEther').value)
+          }
+        >
+          Donate Ether
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='number'
+          id='donateMyGov'
+          name='donateMyGov'
+          placeholder='donateMyGov'
+        />
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            donateMyGovHandler(document.getElementById('donateMyGov').value)
+          }
+        >
+          Donate MyGov
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='number'
+          id='projectid_voteproposal'
+          name='projectid'
+          placeholder='projectid'
+        />
+        <FormControl sx={{ width: 220 }}>
+          <InputLabel id='demo-simple-select-label'>Choice</InputLabel>
+
+          <Select
+            id='demo-simple-select'
+            value={voteChoice}
+            label='Choice'
+            onChange={handleVoteChange}
+          >
+            <MenuItem value={true}>True</MenuItem>
+            <MenuItem value={false}>False</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            voteForProjectProposalHandler(
+              document.getElementById('projectid_voteproposal').value,
+              voteChoice
+            )
+          }
+        >
+          Vote For Project Proposal
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='number'
+          id='projectid_votepayment'
+          name='projectid'
+          placeholder='projectid'
+        />
+        <FormControl sx={{ width: 220 }}>
+          <InputLabel id='demo-simple-select-label'>Choice</InputLabel>
+
+          <Select
+            id='demo-simple-select'
+            value={votePaymentChoice}
+            label='Choice'
+            onChange={handleVotePaymentChange}
+          >
+            <MenuItem value={true}>True</MenuItem>
+            <MenuItem value={false}>False</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            voteForProjectPaymentHandler(
+              document.getElementById('projectid_votepayment').value,
+              votePaymentChoice
+            )
+          }
+        >
+          Vote For Project Payment
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='text'
+          id='delegateaddress'
+          name='delegateaddress'
+          placeholder='delegateaddress'
+        />
+        <TextField
+          type='number'
+          id='projectid'
+          name='projectid'
+          placeholder='projectid'
+        />
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            delegateVoteToHandler(
+              document.getElementById('delegateaddress').value,
+              document.getElementById('projectid').value
+            )
+          }
+        >
+          Delegate Vote To
+        </Button>
+      </form>
+      <form>
+        <TextField
+          type='text'
+          id='ipfshash_submitsurvey'
+          name='ipfshash'
+          placeholder='ipfshash'
+        />
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
+            label='Survey Deadline'
+            inputFormat='DD/MM/YYYY'
+            value={surveyTimeValue}
+            id='surveyDeadline'
+            onChange={handleSurveyTimeChange}
+            renderInput={(params) => <TextField {...params} />}
+            minDate={dateNow}
+          />
+        </LocalizationProvider>
+        <TextField
+          type='number'
+          id='numchoices'
+          name='numchoices'
+          placeholder='numchoices'
+        />
+        <TextField
+          type='number'
+          id='atmostchoice'
+          name='atmostchoice'
+          placeholder='atmostchoice'
+        />
+        <Button
+          onClick={(event) =>
+            event.preventDefault() ||
+            submitSurveyHandler(
+              document.getElementById('ipfshash_submitsurvey').value,
+              surveyTimeDiff,
+              document.getElementById('numchoices').value,
+              document.getElementById('atmostchoice').value
+            )
+          }
+        >
+          Submit Survey
         </Button>
       </form>
     </div>
